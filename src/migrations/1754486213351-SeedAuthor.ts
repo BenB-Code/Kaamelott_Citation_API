5a730ccc5cace27d54ea6e4b6dbdd43ebf77ff65
Benjamin <benjamin.bats.dev@gmail.com>
2025-08-17 20:17:00 +0200

    Adding author migration

---
+++ src/migrations/1754486213351-SeedAuthor.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SeedAuthor1754486213351 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      INSERT INTO "author" ("firstName", "lastName") VALUES 
+      ('Alexandre ', 'Astier'),
+      ('Fabien ', 'Rault'),
+      ('Lionnel ', 'Astier'),
+      ('Nicolas ', 'Gabion'),
+      ('Simon ', 'Astier'),
+      ON CONFLICT DO NOTHING
+    `);
+
+    console.log('✅ Author insérés avec succès');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DELETE FROM "author" WHERE ("firstName", "lastName") IN (
+        ('Alexandre ', 'Astier'),
+        ('Fabien ', 'Rault'),
+        ('Lionnel ', 'Astier'),
+        ('Nicolas ', 'Gabion'),
+        ('Simon ', 'Astier')
+      )
+    `);
+
+    console.log('🗑️ Author supprimés');
+  }
+}
