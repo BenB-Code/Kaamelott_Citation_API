63f9a0bba91d735d8808bd7253b9d6ead6dd4302
Benjamin <benjamin.bats.dev@gmail.com>
2025-08-18 19:59:08 +0200

    Prep migrations for character actors link, episodes and citations

---
+++ src/migrations/1754486200137-SeedCharacterActor.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SeedCharacterActor1754486200137 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
