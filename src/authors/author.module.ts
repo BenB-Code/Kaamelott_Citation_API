cc0662b8dd565f2d5fd97bdfce9da239d5b45428
Benjamin <benjamin.bats.dev@gmail.com>
2025-08-27 17:42:56 +0200

    remove unused logger call

--- src/authors/author.module.ts
+++ src/authors/author.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseExceptions } from './../common/exceptions/database-exceptions.service';
-import { LogLevelEnum } from './../common/logger/models/log-level.enum';
-import { Logger } from './../common/logger/services/logger.service';
 import { AuthorController } from './controllers/author.controller';
 import { Author } from './entities/author.entity';
 import { AuthorRepository } from './repositories/author.repository';
@@ -11,18 +9,6 @@ import { AuthorService } from './services/author.service';
 @Module({
   controllers: [AuthorController],
   imports: [TypeOrmModule.forFeature([Author])],
-  providers: [
-    AuthorService,
-    AuthorRepository,
-    {
-      provide: Logger,
-      useFactory: () =>
-        Logger.getInstance({
-          logLevel: LogLevelEnum.DEBUG,
-          colorize: true,
-        }),
-    },
-    DatabaseExceptions,
-  ],
+  providers: [AuthorService, AuthorRepository, DatabaseExceptions],
 })
 export class AuthorModule {}
